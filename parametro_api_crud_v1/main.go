226079a099cebd023f4502dfb57d0e9661b79d46
felimarod <felipemartinrod@gmail.com>
2023-12-29 16:45:02 -0500

    feat: parametro api crud inicial

---
+++ parametro_api_crud_v1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	_ "github.com/felimarod/ejercicio_crud_mid_api/parametro_api_crud_v1/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
